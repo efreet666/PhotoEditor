9e2e6ecea5803c721db14609715c7a712ba1ba0e
Vlad <vladislav.bok@yandex.ru>
2022-05-16 18:12:54 +0300

    Initial Commit

---
+++ PhotoEditor/PhotoEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEditorApp.swift
+//  PhotoEditor
+//
+//  Created by Влад Бокин on 16.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct PhotoEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
